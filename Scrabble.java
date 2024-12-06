7df2de5c5ae4d0bb2beda053b62a088f075d37bc
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-12-06 17:45:12 +0200

    (.)

--- Scrabble.java
+++ Scrabble.java
@@ -98,7 +98,7 @@ public class Scrabble {
 			String input = in.readString();
 			if(input.equals(".")){break;}
 			else if(!MyString.subsetOf(input, hand)){System.out.println("Invalid word. Try again");}
-			else if(!isWordInDictionary(input)){System.out.println("No such word in the dictionary. Try again");}
+			else if(!isWordInDictionary(input)){System.out.println("No such word in the dictionary. Try again.");}
 			else {
 				score += wordScore(input);
 				System.out.println(input + " earned "+wordScore(input) + " points. Score: " + score + " points" );
